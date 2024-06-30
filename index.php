06a97cbe55b74c929b022fb5994f3583ea1d98ca
CodeYouness <ylijassi@gmail.com>
2024-06-30 12:52:15 +0200

    fixed style of logout button and added bootstrap

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-echo $_SESSION['username'];
-echo $_SESSION['password'];
 
 if (isset($_POST['logout'])) {
     header('Location: ./login.php');
@@ -16,14 +14,22 @@ if (isset($_POST['logout'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
 <body>
-    <main>
-        <form method="post">
-            <input type="submit" name="logout" value="Logout" />
-        </form>
+    <main class="d-flex justify-content-between container">
+        <div>
+            <p>Benvenuto
+                <?php echo $_SESSION['username'] ?>
+            </p>
+        </div>
+        <div>
+            <form method="post">
+                <input type="submit" name="logout" value="Logout" />
+            </form>
+        </div>
     </main>
 </body>
 
