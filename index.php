03945dc875b5a923e5540e2f59292d6ed6ab1200
CodeYouness <ylijassi@gmail.com>
2024-10-28 14:31:42 +0100

    added control if session_username exist

--- index.php
+++ index.php
@@ -14,7 +14,8 @@ if (isset($_POST['logout'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
@@ -22,7 +23,7 @@ if (isset($_POST['logout'])) {
     <main class="d-flex justify-content-between container">
         <div>
             <p>Benvenuto
-                <?php echo $_SESSION['username'] ?>
+                <?php echo isset($_SESSION['username']) ? $_SESSION['username'] : 'Ospite' ?>
             </p>
         </div>
         <div>
